77a8ced0592ac8b7baf5a86d40de17011cd57ad2
Christoper Mercado <christoper_m15@yahoo.com>
2020-04-07 18:52:13 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -5,7 +5,7 @@ var z = [y, ...x];
 return z;
 }
 
-var function destructivelyAddElementToBeginningOfArray(x,y) {
+function destructivelyAddElementToBeginningOfArray(x,y) {
 x.unshift(y);
 return x;
 }
