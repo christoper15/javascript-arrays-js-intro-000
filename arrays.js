f3b345f6a2bdbc34aaf9d87f750f6b4a07d6dea1
Christoper Mercado <christoper_m15@yahoo.com>
2020-04-07 17:29:44 +0000

    Automatically backed up by Learn

---
+++ arrays.js
@@ -0,0 +1,19 @@
+var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles']
+
+function addElementToBeginningOfArray(x,y) {
+var z = [y, ...x];
+return z;
+}
+
+function destructivelyAddElementToBeginningOfArray(x,y) {
+return x.unshift(y);
+}
+
+function addElementToEndOfArray(x,y) {
+var z = [x, ...y];
+return z;
+}
+
+function destructivelyAddElementToEndOfArray(x,y) {
+return x.push(y);
+}
